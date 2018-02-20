019cadb2c289ad70cc22b0aba1985dbc9ba3c5ce
Noel Heesen <noelheesen@gmail.com>
2018-02-20 14:25:06 +0100

    fix(server): Include activities in board subscription

--- app/packages/partup-server/publications/boards.js
+++ app/packages/partup-server/publications/boards.js
@@ -4,10 +4,11 @@ Meteor.publishComposite('board.for_partup_id', function(partupId) {
     var partup = Partups.findOne(partupId);
 
     return {
-        find: function() {
+        find() {
             return Boards.findForPartup(partup, this.userId);
         }, children: [
-            {find: Lanes.findForBoard}
+            {find: Lanes.findForBoard},
+            {find: () => Activities.find({ partup_id: partupId, deleted_at: { $ne: true } })},
         ]
     };
 });
