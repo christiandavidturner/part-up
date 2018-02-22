1846439422fcd89fa9f74d4a954b7908c04885d7
Noel Heesen <noelheesen@gmail.com>
2018-02-22 15:00:44 +0100

    fix(client): Wait for update in single update view

--- app/packages/partup-client-pages/app/partup/update/update.js
+++ app/packages/partup-client-pages/app/partup/update/update.js
@@ -2,14 +2,21 @@ import { isString } from 'lodash';
 
 Template.app_partup_update.onCreated(function() {
   const template = this;
+
+  this.loading = new ReactiveVar(true);
   if (isString(this.data.updateId)) {
     // The update may already be present from caching
     // This will be the case when a user comes from a different route.
     // This will not be the case when a user directs here via a notification
     if (!Updates.findOne(this.data.updateId)) {
-      this.subscribe('updates.one', this.data.updateId);
+      this.subscribe('updates.one', this.data.updateId, {
+        onReady: () => {
+          this.loading.set(false);
+        }
+      });
+    } else {
+      this.loading.set(false);
     }
-
     // Reset new comments for current user
     Meteor.call('updates.reset_new_comments', this.data.updateId);
   }
@@ -22,4 +29,7 @@ Template.app_partup_update.helpers({
   isAnotherDay: function(date) {
     return Partup.client.moment.isAnotherDay(moment(), moment(date));
   },
+  loading() {
+    return Template.instance().loading.get();
+  }
 });
