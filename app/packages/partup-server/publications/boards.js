ce91c7b0f17fec837ad9635b61ea6a4a6031bf37
Noel Heesen <noelheesen@gmail.com>
2018-02-26 14:36:55 +0100

    fix(subscription): Add updates to board subscription

--- app/packages/partup-server/publications/boards.js
+++ app/packages/partup-server/publications/boards.js
@@ -10,9 +10,10 @@ Meteor.publish('board.for_partup_id', function(partupId) {
     const boardCursor = Boards.find({ partup_id: partupId }, { limit: 1 });
     const laneCursor = Lanes.find({ board_id: partup.board_id });
     const activityCursor = Activities.find({ partup_id: partupId });
+    const updatesCursor = Updates.find({ partup_id: partupId, 'type_data.activity_id': { $exists: true } });
     const contributionCursor = Contributions.find({ partup_id: partupId });
 
-    return [boardCursor, laneCursor, activityCursor, contributionCursor];
+    return [boardCursor, laneCursor, activityCursor, contributionCursor, updatesCursor];
   } else {
     return this.ready();
   }
